dcd3cf347b07502f0763204e11de8a58a5f13a67
SIDIKI F <39869497+sidikif@users.noreply.github.com>
2023-09-03 20:24:09 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 DOCKERBIN=docker
 PROJECTNAME=dlms-access-point
-REPO=git@github.com:epri-dev/$PROJECTNAME.git#main
-"$DOCKERBIN" build --target builder -t "beroset/$PROJECTNAME-builder" "$REPO"
-"$DOCKERBIN" build --target docserver -t "beroset/$PROJECTNAME-docs" "$REPO:src/docker/docserver"
-"$DOCKERBIN" build --target demo -t "beroset/$PROJECTNAME" "$REPO:src/docker/demo"
-"$DOCKERBIN" build --target dashboard -t "beroset/$PROJECTNAME-dashboard" "$REPO:src/docker/dashboard"
+REPO=git@github.com:sidikif/$PROJECTNAME.git#main
+"$DOCKERBIN" build --target builder -t "sidikif/$PROJECTNAME-builder" "$REPO"
+"$DOCKERBIN" build --target docserver -t "sidikif/$PROJECTNAME-docs" "$REPO:src/docker/docserver"
+"$DOCKERBIN" build --target demo -t "sidikif/$PROJECTNAME" "$REPO:src/docker/demo"
+"$DOCKERBIN" build --target dashboard -t "sidikif/$PROJECTNAME-dashboard" "$REPO:src/docker/dashboard"
